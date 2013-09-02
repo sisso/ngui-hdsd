4a3c25babc1c78fd3dd0dd29e7d4592fbb2cd558
sisso <sisso@sisso.com.br>
2013-09-02 16:41:08 -0300

    - change build script

--- Assets/Scripts/Editor/BuildWindowEditor.cs
+++ Assets/Scripts/Editor/BuildWindowEditor.cs
@@ -5,10 +5,37 @@ using System.Collections;
 public class BuildWindowEditor : EditorWindow {
 	[MenuItem("Window/Prepare Build")]
 	public static void PrepareBuild() {
+		ClearAtlasRef();
+		PrepareScenes();
+		
+		EditorApplication.OpenScene("Assets/Scenes/SplashScene.unity");
+	}
+	
+	private static void ClearAtlasRef() {
 		EditorApplication.NewScene();
 		
 		var atlasRef = Resources.Load("Atlas/AtlasRef", typeof(UIAtlas)) as UIAtlas;
 		atlasRef.replacement = null;
 		atlasRef.spriteMaterial = null;
+	}
+	
+	private static void PrepareScenes() {
+		EditorApplication.OpenScene("Assets/Scenes/FirstScene.unity");
+		ClearWidgets();
+		EditorApplication.SaveScene();
+		
+		EditorApplication.OpenScene("Assets/Scenes/SecondScene.unity");
+		ClearWidgets();
+		EditorApplication.SaveScene();
+	}
+	
+	private static void ClearWidgets() {
+		var widgets = GameObject.FindObjectsOfType(typeof(UIWidget));
+		foreach (UIWidget w in widgets) {
+			w.enabled = false;
+			w.enabled = true;
+			w.gameObject.SetActive(false);
+			w.gameObject.SetActive(true);
+		}
 	}
 }
