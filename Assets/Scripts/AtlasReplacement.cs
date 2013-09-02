e465b753d37853e6b8bfe5b7471bfddf9789992d
sisso <sisso@sisso.com.br>
2013-09-02 16:10:38 -0300

    - v0

---
+++ Assets/Scripts/AtlasReplacement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtlasReplacement : MonoBehaviour {
+	public UIAtlas atlasRef;
+	
+	public string pathAtlasSd;
+	
+	public bool sd = true;
+	
+	void Awake() {
+		var sdAtlas = Resources.Load(pathAtlasSd, typeof(UIAtlas)) as UIAtlas;
+		atlasRef.replacement = sdAtlas;
+	}
+}
---
+++ Assets/Scripts/Editor/BuildWindowEditor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BuildWindowEditor : EditorWindow {
+	[MenuItem("Window/Prepare Build")]
+	public static void PrepareBuild() {
+	}
+}
