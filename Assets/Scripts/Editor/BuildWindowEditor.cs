77ca012e9f8888f6e2d68f5d296285777a15000f
sisso <sisso@sisso.com.br>
2013-09-02 17:51:18 -0300

    - clean up prefabs

--- Assets/Scripts/Editor/BuildWindowEditor.cs
+++ Assets/Scripts/Editor/BuildWindowEditor.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 public class BuildWindowEditor : EditorWindow {
 	[MenuItem("Window/Prepare Build")]
 	public static void PrepareBuild() {
 		ClearAtlasRef();
 		PrepareScenes();
+		PreparePrefabs();
 		
 		EditorApplication.OpenScene("Assets/Scenes/SplashScene.unity");
 	}
@@ -34,8 +37,45 @@ public class BuildWindowEditor : EditorWindow {
 		foreach (UIWidget w in widgets) {
 			w.enabled = false;
 			w.enabled = true;
-			w.gameObject.SetActive(false);
-			w.gameObject.SetActive(true);
 		}
+	}
+	
+	private static void PreparePrefabs() {
+		foreach(var p in FindPaths(new string[] { "*.prefab" })) {
+			EditorApplication.NewScene();
+			var prefab = AssetDatabase.LoadMainAssetAtPath(p);
+			var obj = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+			var dirt = false;
+			
+			var widgets = obj.GetComponentsInChildren(typeof(UIWidget));
+			foreach (UIWidget w in widgets) {
+				w.enabled = false;
+				w.enabled = true;
+				
+				dirt = true;
+			}
+			
+			if (dirt) {
+				Debug.Log("Updating "+p);
+				PrefabUtility.ReplacePrefab(obj, prefab, ReplacePrefabOptions.Default);
+			}
+			GameObject.DestroyImmediate(obj);
+		}
+	}
+	
+	private static string[] FindPaths(string[] filters) {
+ 		var paths = new List<string>();    	
+ 		foreach(var filter in filters) {
+			var files = Directory.GetFiles(Application.dataPath, filter, SearchOption.AllDirectories);
+			foreach(var f in files) {
+				paths.Add(NormalizeDataPath(f));
+			}
+		}
+		return paths.ToArray();
+	}
+	
+	private static string NormalizeDataPath(string path) {
+		var normalizedPath = "Assets" + path.Replace(Application.dataPath, "").Replace('\\', '/');
+		return normalizedPath;
 	}
 }
---
+++ Assets/Scripts/Factory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Factory : MonoBehaviour {
+	public GameObject prefab;
+	
+	void Start () {
+		NGUITools.AddChild(gameObject, prefab);
+	}
+}
